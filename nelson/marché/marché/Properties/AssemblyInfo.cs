b7166853649ec33da504a2a4d505e97b49080a7a
Nelson Almeida <pf14wzd@eduvaud.ch>
2025-08-28 09:35:09 +0200

    feat(marché) Initial commit

--- nelson/marché/marché/Properties/AssemblyInfo.cs
+++ nelson/marché/marché/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
-[assembly: Guid("2a58898d-f119-42b9-8b5e-e2268e3e5cda")]
+[assembly: Guid("f23a5f72-c0b4-4377-82d2-2c3059cb368a")]
 
 // Les informations de version pour un assembly se composent des quatre valeurs suivantes :
 //
